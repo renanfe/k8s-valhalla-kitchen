06557335945d7973248c129a64af9cdd27fca5cf
Renan Felipe Teixeira <renan.felipe11@gmail.com>
2023-11-03 17:37:32 -0300

    Criação da vpc e eks

---
+++ terraform/tr-valhalla-kitchen.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.2"
+
+  name = "vpc-valhalla-kitchen"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "17.1.0"
+
+  cluster_name    = "cluster-valhalla-kitchen"
+  cluster_version = "1.28"
+  subnets         = module.vpc.public_subnets
+  vpc_id          = module.vpc.vpc_id
+
+  node_groups = {
+    eks_nodes = {
+      desired_capacity = 2
+      max_capacity     = 10
+      min_capacity     = 1
+
+      instance_type = "m5.large"
+      key_name      = "my-key"
+      
+      additional_tags = {
+        Environment = "test"
+        Name        = "eks-worker-node"
+      }
+    }
+  }
+}

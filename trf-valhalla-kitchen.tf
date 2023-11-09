da9f61e8c0c07db200677dd8c17a445ed7668147
Renan Felipe Teixeira <renan.felipe11@gmail.com>
2023-11-08 21:55:25 -0300

    alterado terraform

--- trf-valhalla-kitchen.tf
+++ trf-valhalla-kitchen.tf
@@ -27,8 +27,6 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = true
-  enable_dns_hostnames = true
   public_subnet_tags = {
     "kubernetes.io/cluster/my_eks" = "shared"
     "kubernetes.io/role/internal-elb" = 1

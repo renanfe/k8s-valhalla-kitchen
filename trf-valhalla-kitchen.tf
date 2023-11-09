916965fe456c5ef33beae1850a7d3ad5a54fd2d6
Renan Felipe Teixeira <renan.felipe11@gmail.com>
2023-11-08 23:18:23 -0300

    rollback

--- trf-valhalla-kitchen.tf
+++ trf-valhalla-kitchen.tf
@@ -27,35 +27,7 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  public_subnet_tags = {
-    "kubernetes.io/cluster/my_eks" = "shared"
-    "kubernetes.io/role/internal-elb" = 1
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/cluster/my_eks" = "shared"
-    "kubernetes.io/role/internal-elb" = 1
-  }
-}
-
-resource "aws_iam_role" "role" {
-  name = "EksValhallaKitchen"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = [
-            "eks.amazonaws.com",
-            "ec2.amazonaws.com"
-          ]
-        }
-      }
-    ]
-  })
+  enable_nat_gateway = true
 }
 
 module "eks" {
@@ -64,31 +36,7 @@ module "eks" {
 
   cluster_name    = "cluster-valhalla-kitchen"
   cluster_version = "1.28"
-
   subnets         = module.vpc.public_subnets
   vpc_id          = module.vpc.vpc_id
 
-  depends_on = [module.vpc]
-  
-}
-
-resource "aws_iam_role_policy_attachment" "policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.role.name
-  depends_on = [aws_iam_role.role]
-}
-
-resource "aws_eks_node_group" "node_group" {
-  cluster_name    = module.eks.cluster_id
-  node_group_name = "valhalla_node_group"
-  node_role_arn   = aws_iam_role.role.arn
-  subnet_ids      = module.vpc.public_subnets
-
-  scaling_config {
-    desired_size = 1
-    max_size     = 1
-    min_size     = 1
-  }
-
-  depends_on = [aws_iam_role_policy_attachment.policy, module.eks]
 }

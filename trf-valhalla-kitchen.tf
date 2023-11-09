9e643f6b6edefdf2f60a6e2e7334c5f36f795fb6
Renan Felipe Teixeira <renan.felipe11@gmail.com>
2023-11-08 22:37:54 -0300

    alterado terraform

--- trf-valhalla-kitchen.tf
+++ trf-valhalla-kitchen.tf
@@ -48,7 +48,10 @@ resource "aws_iam_role" "role" {
         Action = "sts:AssumeRole"
         Effect = "Allow"
         Principal = {
-          Service = "eks.amazonaws.com"
+          Service = [
+            "eks.amazonaws.com",
+            "ec2.amazonaws.com"
+          ]
         }
       }
     ]
